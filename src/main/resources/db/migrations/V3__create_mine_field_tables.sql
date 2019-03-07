d50f82f5415f268cd05bc664c0c2f75c52453766
Todd Crone <twcrone@gmail.com>
2019-03-06 22:47:30 -0500

    Add 'size' for mine field

--- src/main/resources/db/migrations/V3__create_mine_field_tables.sql
+++ src/main/resources/db/migrations/V3__create_mine_field_tables.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS "mine_field"
 (
-  "id" SERIAL PRIMARY KEY
+  "id"   SERIAL PRIMARY KEY,
+  "size" INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "mine"
 (
   "id"            SERIAL PRIMARY KEY,
-  "mine_field_id" BIGINT    NOT NULL,
+  "mine_field_id" BIGINT  NOT NULL,
   "x"             INTEGER NOT NULL,
   "y"             INTEGER NOT NULL,
   "z"             INTEGER NOT NULL,
@@ -14,8 +15,8 @@ CREATE TABLE IF NOT EXISTS "mine"
   FOREIGN KEY ("mine_field_id") REFERENCES "mine_field" ("id")
 );
 
-INSERT INTO mine_field(id)
-VALUES (1);
+INSERT INTO mine_field(id, size)
+VALUES (1, 3);
 
 INSERT INTO mine(id, mine_field_id, x, y, z)
 VALUES (DEFAULT, 1, 1, 1, 1);
