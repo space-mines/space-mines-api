9b1674321650d7d92d40fc6f2f4f1aee42204a1c
Todd Crone <twcrone@gmail.com>
2019-03-06 20:54:17 -0500

    Remove all the web crap

--- src/main/kotlin/com/example/player/PlayerEntity.kt
+++ src/main/kotlin/com/example/player/PlayerEntity.kt
@@ -1,16 +1,12 @@
 package com.example.player
 
-import java.time.Instant
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity(name = "player")
-data class PlayerEntity(
+class PlayerEntity(
         @Id
         val id: Long,
         val username: String,
-        val password: String,
-        val email: String,
-        val createdOn: Instant,
-        val lastLogin: Instant? = null
+        val email: String
 )
