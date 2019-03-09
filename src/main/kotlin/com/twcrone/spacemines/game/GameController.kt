9a1d543255a2168a5fadcf609fe6993a9a7ab162
Todd Crone <twcrone@gmail.com>
2019-03-09 14:11:23 -0500

    Modify POST game for creating a game based on minefield

--- src/main/kotlin/com/twcrone/spacemines/game/GameController.kt
+++ src/main/kotlin/com/twcrone/spacemines/game/GameController.kt
@@ -6,13 +6,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class GameController(private val repository: GameRepository,
+class GameController(private val service: GameService,
                      private val mineFieldRepository: MineFieldRepository) {
 
     @GetMapping("/game/{id}")
     fun getGame(@PathVariable id: Long): ResponseEntity<GameRep> {
         println("Finding game...")
-        val entity = repository.findOne(id)
+        val entity = service.get(id)
         println("Found game with ID=${entity.id}")
         return ResponseEntity(GameRep.fromEntity(entity), HttpStatus.OK)
     }
@@ -22,16 +22,7 @@ class GameController(private val repository: GameRepository,
             produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun createGame(): ResponseEntity<GameRep> {
         val mineField = mineFieldRepository.findOne(1)
-        val game = GameEntity(mineField)
-        val size = mineField.size
-        repeat(size) { x ->
-            repeat(size) { y ->
-                repeat(size) { z ->
-                    game.minePods.add(MinePodEntity(x = x, y = y, z = z))
-                }
-            }
-        }
-        val saved = repository.save(game)
+        val saved = service.create(mineField)
         return ResponseEntity(GameRep.fromEntity(saved), HttpStatus.OK)
     }
 
