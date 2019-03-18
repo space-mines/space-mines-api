73d14f81dd320f835649e34b2b248489cdb2112a
Todd Crone <twcrone@gmail.com>
2019-03-17 21:49:35 -0400

    Fix typo in appliction-dev.properties

--- src/main/kotlin/com/twcrone/spacemines/api/GameController.kt
+++ src/main/kotlin/com/twcrone/spacemines/api/GameController.kt
@@ -7,8 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class GameController(private val service: GameService,
-                     private val mineFieldRepository: LevelRepository) {
+class GameController(private val service: GameService) {
 
     @CrossOrigin
     @GetMapping("/games")
