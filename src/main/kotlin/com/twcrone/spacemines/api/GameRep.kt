d9e5982cbf80f747cc30cea4e4c775df6e8bf894
Todd Crone <twcrone@gmail.com>
2019-03-18 22:12:15 -0400

    Add 'level' field to Game API

--- src/main/kotlin/com/twcrone/spacemines/api/GameRep.kt
+++ src/main/kotlin/com/twcrone/spacemines/api/GameRep.kt
@@ -2,13 +2,14 @@ package com.twcrone.spacemines.api
 
 import com.twcrone.spacemines.data.GameEntity
 
-data class GameRep(val id: Long) {
+data class GameRep(val id: Long,
+                   val level: Long) {
 
     val pods: MutableSet<PodRep> = HashSet()
 
     companion object {
         fun fromEntity(entity: GameEntity): GameRep {
-            val game = GameRep(id = entity.id)
+            val game = GameRep(id = entity.id, level = entity.level!!.id)
             entity.pods.forEach{ minePod ->
                 game.pods.add(PodRep(
                         id = minePod.id,
